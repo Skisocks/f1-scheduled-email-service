a9d05fb8678525e939630ba8d4dc21acf7132f7a
archieskeoch <jaa.skeoch@gmail.com>
2021-12-06 20:19:03 +0000

    Initial commit

---
+++ models/models.go
@@ -0,0 +1,34 @@
+package models
+
+import "time"
+
+type CurrentEvents struct {
+	Response []struct {
+		Id          int `json:"id"`
+		Competition struct {
+			Id       int    `json:"id"`
+			Name     string `json:"name"`
+			Location struct {
+				Country string `json:"country"`
+				City    string `json:"city"`
+			} `json:"location"`
+		} `json:"competition"`
+		Circuit struct {
+			Id    int    `json:"id"`
+			Name  string `json:"name"`
+			Image string `json:"image"`
+		} `json:"circuit"`
+		Season int    `json:"season"`
+		Type   string `json:"type"`
+		Laps   struct {
+			Current interface{} `json:"current"`
+			Total   int         `json:"total"`
+		} `json:"laps"`
+		Distance string      `json:"distance"`
+		Timezone string      `json:"timezone"`
+		Date     time.Time   `json:"date"`
+		Weather  interface{} `json:"weather"`
+		Status   string      `json:"status"`
+	} `json:"response"`
+}
+
