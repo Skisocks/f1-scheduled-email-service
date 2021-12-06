ad4b021ad411cfc7a8bb9d74bea7d91e6115828d
archieskeoch <jaa.skeoch@gmail.com>
2021-12-06 21:41:15 +0000

    feat: add EmailService

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"email-service/clients"
+	"email-service/config"
+	"email-service/service"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -11,5 +15,20 @@ func main() {
 }
 
 func run() error {
+	// Get config struct
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return fmt.Errorf("failed to get config: %s", err)
+	}
 
+	F1APIClient := clients.NewF1APIClient(&cfg.F1APIClient, time.Second * 5)
+
+	EmailService := service.NewEmailService(F1APIClient)
+
+	err := EmailService.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
