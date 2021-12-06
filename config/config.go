8ad983d51ffd62b66834c4f0885af50f660d1466
archieskeoch <jaa.skeoch@gmail.com>
2021-12-06 11:49:45 +0000

    Initial commit

---
+++ config/config.go
@@ -0,0 +1,24 @@
+package config
+
+type Config struct {
+	EmailHandler EmailHandler
+	ErgastClient ErgastClient
+	F1APIClient	F1APIClient
+	Repository	Repository
+}
+
+type EmailHandler struct {
+
+}
+
+type ErgastClient struct {
+
+}
+
+type F1APIClient struct {
+
+}
+
+type Repository struct {
+
+}
