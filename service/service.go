af9955f355978adc55a1c5a38eb58c2aa35c6a14
archieskeoch <jaa.skeoch@gmail.com>
2021-12-06 21:42:11 +0000

    feat: add EmailService

--- service/service.go
+++ service/service.go
@@ -1,1 +1,17 @@
 package service
+
+import (
+	"email-service/clients"
+)
+
+type ScheduleEmailService struct {
+	F1APIClient	*clients.F1API
+}
+
+func NewEmailService(F1APIClient *clients.F1API) *ScheduleEmailService {
+	return &ScheduleEmailService{F1APIClient}
+}
+
+func (es ScheduleEmailService) Run() error {
+
+}
