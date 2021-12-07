4ef6f223450c79d1811a8885781b5f82e09542a3
archieskeoch <jaa.skeoch@gmail.com>
2021-12-07 20:57:25 +0000

    refactor: add dbclient functions

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"email-service/clients"
 	"email-service/config"
+	"email-service/repository"
 	"email-service/service"
 	"fmt"
+	_ "github.com/lib/pq"
+	"log"
 	"time"
 )
 
@@ -23,12 +27,49 @@ func run() error {
 
 	F1APIClient := clients.NewF1APIClient(&cfg.F1APIClient, time.Second*5)
 
+	DB, err := setupDB(&cfg.DatabaseClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Repository := repository.NewRepository(DB)
+
+	Users := Repository.UserEmails()
+	_ = fmt.Sprintf("%s", Users)
+
 	EmailService := service.NewEmailService(F1APIClient)
 
-	EmailService.Run()
+	EmailService.Run(F1APIClient)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func setupDB(cfg *config.Repository) (*sql.DB, error) {
+	// connection string
+	psqlconn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s search_path=%s sslmode=disable",
+		cfg.Host,
+		cfg.Port,
+		cfg.User,
+		cfg.Password,
+		cfg.Name,
+		cfg.Schema,
+	)
+
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		panic(err)
+	}
+	return db, err
+}
+
+func closeDB(DB *sql.DB) error {
+	err := DB.Close()
+	if err != nil {
+		return err
+	}
+	return err
 }
