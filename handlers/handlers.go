e66628503ef3ee527c3cda6e0c2b26f1f02be9a3
archieskeoch <jaa.skeoch@gmail.com>
2021-12-08 19:50:56 +0000

    refactor: rename interface

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,34 +2,30 @@ package handlers
 
 import (
 	"email-service/config"
-	"email-service/repository"
 	"fmt"
 	"gopkg.in/gomail.v2"
-	// "gopkg.in/gomail.v2"
 )
 
-type EmailHandler interface {
-	SendEmail() error
+type EmailSender interface {
+	SendEmail(UserEmails []string) error
 }
 
 type emailHandler struct {
 	config *config.EmailHandler
-	repo   repository.Repository
 }
 
-func NewEmailHandler(cfg *config.EmailHandler, repo repository.Repository) *emailHandler {
+func NewEmailHandler(cfg *config.EmailHandler) *emailHandler {
 	return &emailHandler{
 		config: cfg,
-		repo:   repo,
 	}
 }
 
-func (eh *emailHandler) SendEmail() error {
+func (eh *emailHandler) SendEmail(UserEmails []string) error {
 	m := gomail.NewMessage()
 
 	// Set message headers
 	m.SetAddressHeader("From", eh.config.SenderAddress, "F1 Info")
-	m.SetHeader("To", eh.repo.GetUserEmails()...)
+	m.SetHeader("To", UserEmails...)
 	m.SetHeader("Subject", "Test subject") // Todo: Make subject
 	m.SetBody("text/plain", "Test body")   // Todo: Make body
 
