bcbdb04dd66b7c99613f1b3559b073280bbf9c4e
archieskeoch <jaa.skeoch@gmail.com>
2021-12-08 13:43:30 +0000

    feat: add SendEmail()

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,26 +2,49 @@ package handlers
 
 import (
 	"email-service/config"
-	"email-service/models"
 	"email-service/repository"
+	"fmt"
+	"gopkg.in/gomail.v2"
+	// "gopkg.in/gomail.v2"
 )
 
-type emailHandler interface {
-	SendEmail()
+type EmailHandler interface {
+	SendEmail() error
 }
 
-type EmailHandler struct {
+type emailHandler struct {
 	config *config.EmailHandler
-	repo  repository.Repository
+	repo   repository.Repository
 }
 
-func NewEmailHandler(cfg *config.EmailHandler, repo repository.Repository) *EmailHandler {
-	return &EmailHandler{
+func NewEmailHandler(cfg *config.EmailHandler, repo repository.Repository) *emailHandler {
+	return &emailHandler{
 		config: cfg,
-		repo:  users,
+		repo:   repo,
 	}
 }
 
-func (eh *EmailHandler) SendEmail() {
+func (eh *emailHandler) SendEmail() error {
+	m := gomail.NewMessage()
 
+	// Set message headers
+	m.SetAddressHeader("From", eh.config.SenderAddress, "F1 Info")
+	m.SetHeader("To", eh.repo.GetUserEmails()...)
+	m.SetHeader("Subject", "Test subject") // Todo: Make subject
+	m.SetBody("text/plain", "Test body") // Todo: Make body
+
+	// SMTP settings
+	d := gomail.NewDialer(
+		eh.config.SMTPServer,
+		eh.config.SMTPHost,
+		eh.config.SenderAddress,
+		eh.config.SenderPassword,
+		)
+
+	// Send email
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	return nil
 }
