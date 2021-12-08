9f654da17d89a3cfc31fcd94a8b2d4c705e0d347
archieskeoch <jaa.skeoch@gmail.com>
2021-12-08 14:07:21 +0000

    refactor: add EmailHandler

--- service/service.go
+++ service/service.go
@@ -2,23 +2,29 @@ package service
 
 import (
 	"email-service/clients"
+	"email-service/handlers"
 	"email-service/repository"
 )
 
 type ScheduleEmailService struct {
-	SportsIOClient *clients.SportsIO
-	ErgastClient   *clients.Ergast
-	Repository     repository.Repository
+	sportsIOClient *clients.SportsIO
+	ergastClient   *clients.Ergast
+	repository     repository.Repository
+	emailHandler   handlers.EmailHandler
 }
 
 func NewEmailService(
 	SportsIOClient *clients.SportsIO,
 	ErgastClient *clients.Ergast,
-	Repository repository.Repository, ) *ScheduleEmailService {
+	Repository repository.Repository,
+	EmailHandler handlers.EmailHandler,
+) *ScheduleEmailService {
 	return &ScheduleEmailService{
 		SportsIOClient,
 		ErgastClient,
-		Repository}
+		Repository,
+		EmailHandler,
+	}
 }
 
 func (es ScheduleEmailService) Run() {
