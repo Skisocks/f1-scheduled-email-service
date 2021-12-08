77ce3e2f74c54983b4b585e92beb54c7ce89e4d0
archieskeoch <jaa.skeoch@gmail.com>
2021-12-08 16:20:24 +0000

    refactor: split clients into seperate files

--- clients/sportsio.go
+++ clients/sportsio.go
@@ -10,40 +10,8 @@ import (
 	"time"
 )
 
-type Client interface {
-	do(string, string, map[string]string) (*http.Response, error)
-}
-
-type Ergast struct {
-	config     *config.Ergast
-	httpClient *http.Client
-}
-
-func NewErgastClient(cfg *config.Ergast, timeout time.Duration) *Ergast {
-	return &Ergast{
-		config:     cfg,
-		httpClient: &http.Client{Timeout: timeout},
-	}
-}
-
-func (er *Ergast) Do(method string, endpoint string, params map[string]string) (*http.Response, error) {
-	// Create new request
-	baseURL := fmt.Sprintf("%s/%s%s", er.config.BaseURL, er.config.Season, endpoint)
-	req, err := http.NewRequest(method, baseURL, nil)
-	if err != nil {
-		// Todo: handle error
-		return nil, err
-	}
-
-	// Iterate through params and add to query
-	q := req.URL.Query()
-	for key, val := range params {
-		q.Set(key, val)
-	}
-	// Add query to request
-	req.URL.RawQuery = q.Encode()
-
-	return er.httpClient.Do(req)
+type CurrentEventGetter interface {
+	GetCurrentEvents() (CurrentEvents *models.CurrentEvents, err error)
 }
 
 type SportsIO struct {
