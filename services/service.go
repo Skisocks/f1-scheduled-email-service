19b69b4620749fd53effa525b00672dc0b2f594d
archieskeoch <jaa.skeoch@gmail.com>
2021-12-09 11:49:35 +0000

    feat: add getEvent()

--- services/service.go
+++ services/service.go
@@ -10,9 +10,9 @@ import (
 
 type ScheduleEmailService struct {
 	sportsIOClient clients.CurrentEventGetter
-	ergastClient clients.StandingsGetter
-	repository   repositories.UserEmailGetter
-	emailHandler handlers.EmailSender
+	ergastClient   clients.StandingsGetter
+	repository     repositories.UserEmailGetter
+	emailHandler   handlers.EmailSender
 }
 
 func NewEmailService(
@@ -31,37 +31,44 @@ func NewEmailService(
 
 func (ses ScheduleEmailService) Run() {
 	eventsResponse, _ := ses.sportsIOClient.GetEventsResponse()
-	if isEvent(eventsResponse) == true {
-		emailSubject := fmt.Sprintf("The %s session of the %s today starts at %s", ) //
-		emailBody := fmt.Sprintf("")
+	todaysEvent := getEvent(eventsResponse)
 
+	if todaysEvent != nil {
+		emailSubject := fmt.Sprintf(
+			"The %s session of the %s today starts at %s",
+			todaysEvent.Type,
+			todaysEvent.Name,
+			todaysEvent.Datetime.Format("15:04:05 MST"),
+		)
+		emailBody := fmt.Sprintf("")
 
 		// Send email to the current users
 		err := ses.emailHandler.SendEmail(emailSubject, emailBody, ses.repository.GetUserEmails())
 		if err != nil {
 			return
 		}
-
-
 	}
 }
 
-// isEvent takes a models.EventsResponse and returns a boolean
-// depending on whether there is a qualifying session or a race today.
-func isEvent(EventsResponse *models.EventsResponse) bool {
-	isEvent := false
+// getEvent takes a models.EventsResponse it returns a models.CurrentEvent
+// depending on whether there is a race or 1st qualifying session today.
+// If getEvent == nil then there is no event today.
+func getEvent(EventsResponse *models.EventsResponse) *models.CurrentEvent {
+	var todaysEvent *models.CurrentEvent = nil
 	for i := range EventsResponse.Events {
+		// Check if there's a race or 1st qualifying today
 		switch EventsResponse.Events[i].Type {
 		case "Race", "1st Qualifying":
-			isEvent = true
+			{
+				// Take the required variables from the EventResponse and add them to todaysEvent
+				todaysEvent = &models.CurrentEvent{
+					Name:     EventsResponse.Events[i].Competition.Name,
+					Type:     EventsResponse.Events[i].Type,
+					Datetime: EventsResponse.Events[i].Date,
+				}
+				return todaysEvent
+			}
 		}
 	}
-	return isEvent
-}
-
-func GetEvent(EventsResponse *models.EventsResponse) models.Event {
-	for i := range EventsResponse.Events {
-		}
-	}
-	return
+	return todaysEvent
 }
